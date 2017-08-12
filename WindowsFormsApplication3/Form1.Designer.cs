2724be87c36230b68ca50b9574eafcc34922d78a
wl4837 <wl4837@163.com>
2017-08-12 17:41:55 +0800

    Add files via upload

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.父级地址ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.密码ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.资源地址ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(528, 358);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "类型";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "父地址";
+            this.columnHeader2.Width = 95;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "资源地址";
+            this.columnHeader3.Width = 94;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "密码";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(61, 152);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(727, 358);
+            this.splitContainer1.SplitterDistance = 528;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 344);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(528, 14);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.Size = new System.Drawing.Size(195, 358);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "搜索一下";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(620, 42);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer2.Location = new System.Drawing.Point(61, 73);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.button1);
+            this.splitContainer2.Size = new System.Drawing.Size(727, 42);
+            this.splitContainer2.SplitterDistance = 620;
+            this.splitContainer2.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.父级地址ToolStripMenuItem,
+            this.资源地址ToolStripMenuItem,
+            this.密码ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 70);
+            // 
+            // 父级地址ToolStripMenuItem
+            // 
+            this.父级地址ToolStripMenuItem.Name = "父级地址ToolStripMenuItem";
+            this.父级地址ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.父级地址ToolStripMenuItem.Text = "父级地址";
+            this.父级地址ToolStripMenuItem.Click += new System.EventHandler(this.父级地址ToolStripMenuItem_Click);
+            // 
+            // 密码ToolStripMenuItem
+            // 
+            this.密码ToolStripMenuItem.Name = "密码ToolStripMenuItem";
+            this.密码ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.密码ToolStripMenuItem.Text = "密码";
+            this.密码ToolStripMenuItem.Click += new System.EventHandler(this.密码ToolStripMenuItem_Click);
+            // 
+            // 资源地址ToolStripMenuItem
+            // 
+            this.资源地址ToolStripMenuItem.Name = "资源地址ToolStripMenuItem";
+            this.资源地址ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.资源地址ToolStripMenuItem.Text = "资源地址";
+            this.资源地址ToolStripMenuItem.Click += new System.EventHandler(this.资源地址ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 544);
+            this.Controls.Add(this.splitContainer2);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "资源搜索";
+            this.AutoSizeChanged += new System.EventHandler(this.Form1_AutoSizeChanged);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 父级地址ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 密码ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 资源地址ToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Web;
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        void zhidao()//百度知道爬虫
+        {
+            int jmp = 1;
+            int ax = 0;
+            int Lan = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                WebClient wc = new WebClient();
+                string html = wc.DownloadString(string.Format("https://zhidao.baidu.com/search?word={0}&ie=gbk&site=-1&sites=0&date=2&pn={0}0", textBox1.Text,i));
+                //测试
+                //MessageBox.Show(html);
+
+                MatchCollection matches = Regex.Matches(html, "http[s]?://zhidao.baidu.com/question/([\\s\\S]*?).html");
+               
+                progressBar1.Maximum += matches.Count * 10;
+                foreach (Match item in matches)
+                {
+                    ax++;
+                    if (jmp >= 1)
+                    {
+                        jmp = 0;
+                        Lan += baiduyun(item.Value.ToString(), "百度知道");
+                    }
+                    else
+                    {
+                        jmp++;
+                    }
+                    progressBar1.Value += 10;
+                }
+            }
+            richTextBox1.Text += string.Format("共抓取百度知道"+ax+"个父地址\n共提取到"+Lan+"个百度云地址\n");
+          
+        }
+
+        //正则表达式筛选模块
+       string help(MatchCollection add) {
+             string url = "";
+            foreach (Match vim in add)
+            {
+                url = vim.Value.ToString();
+
+            }
+             return url;
+        }
+       
+        //百度云爬取模块
+       int baiduyun(string item,string type){
+           int L = 0;
+             WebClient wc = new WebClient();
+             string html = wc.DownloadString(item);
+             MatchCollection url = Regex.Matches(html, "http([s]{0,1})://pan.baidu.com/[a-zA-Z0-9]{1,3}/([\\s\\S\\n]{1,25})");
+             foreach (Match python in url)
+             {
+                 MatchCollection pw = Regex.Matches(python.Value.ToString(), "([密码]{1,3})([:：]{1,1})([a-zA-z0-9]{4,4})");
+                 string password = help(pw);
+                 MatchCollection ul = Regex.Matches(python.Value.ToString(), "http([s]{0,1})://pan.baidu.com/[a-zA-Z0-9]{1,3}/([a-zA-Z0-9]{1,10})");
+                 string urladd = help(ul);
+                 ListViewItem items = new ListViewItem(type);
+                 items.SubItems.Add(item);
+                 items.SubItems.Add(urladd);
+                 items.SubItems.Add(password);
+                 listView1.Items.Add(items);
+                 L++;
+             }
+             return L;
+         }
+        void tieba() //百度贴吧爬虫    
+        {
+            int jmp = 1;
+            int ax = 0;
+            int Lan = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                WebClient wc = new WebClient();
+                Encoding GBK = Encoding.GetEncoding("GBK");
+                string html = wc.DownloadString(string.Format("http://tieba.baidu.com/f/search/res?ie=utf-8&isnew=1&kw=&qw={0}&rn=10&un=&only_thread=0&sm=1&sd=&ed=&pn={1}", textBox1.Text,i));
+                Encoding.GetEncoding("GBK").GetString(Encoding.Default.GetBytes(html));
+                MatchCollection matches = Regex.Matches(html, "/p/([0-9]+)\\?pid=([0-9]+)&cid=([0-9#]+)");
+                
+                progressBar1.Maximum += matches.Count * 10;
+                foreach (Match item in matches)
+                {
+                    ax++;
+                    if (jmp >= 1)
+                    {
+                        jmp = 0;
+                        //richTextBox1.Text += string.Format("https://tieba.baidu.com" + item.Value.ToString()+"\n");   //测试语句
+                        Lan += baiduyun(string.Format("https://tieba.baidu.com" + item.Value.ToString()), "百度贴吧");
+                    }
+                    else
+                    {
+                        jmp++;
+                    }
+                    progressBar1.Value += 10;
+                }
+                
+            }
+            richTextBox1.Text += string.Format("共抓取百度贴吧" + ax + "个父地址-共提取到" + Lan + "个百度云地址\n");
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 0;
+            listView1.Items.Clear();//清空上次查询
+            richTextBox1.Text = null;
+            //zhidao();
+            Thread tiebaThread = new Thread(new ThreadStart(tieba));
+            tiebaThread.Start();
+            Thread zhidaoThread = new Thread(new ThreadStart(zhidao));
+            zhidaoThread.Start();
+            //while (conut>=2)
+            //{
+            //    progressBar1.Maximum = id;
+            //    richTextBox1.Text += id.ToString();
+            //}
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count>0)
+            {
+                System.Diagnostics.Process.Start(listView1.SelectedItems[0].SubItems[2].Text.ToString());
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_AutoSizeChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_VisibleChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void 父级地址ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                System.Diagnostics.Process.Start(listView1.SelectedItems[0].SubItems[1].Text.ToString());
+
+            }
+        }
+
+        private void 资源地址ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                System.Diagnostics.Process.Start(listView1.SelectedItems[0].SubItems[2].Text.ToString());
+
+            }
+        }
+
+        private void 密码ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                Clipboard.SetDataObject(listView1.SelectedItems[0].SubItems[3].Text.ToString());
+            }
+        }
+    }
+}
